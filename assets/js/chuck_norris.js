8e986f1a9b587fc1125b1ddfdf3b732a742fe6ac
stella <nerajno@gmail.com>
2018-05-20 19:28:14 -0400

    added the api an made it console.log

--- assets/js/chuck_norris.js
+++ assets/js/chuck_norris.js
@@ -1,9 +1,19 @@
 // this should be fun
+var humbleRequest = new XMLHttpRequest();
+humbleRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
+humbleRequest.onload = function(){
+    var humbleData = JSON.parse(humbleRequest.responseText);
+    console.log(humbleData.value);
+};
+humbleRequest.send();
+// Begin accessing JSON data here
 
-$.ajax({
-  url: 'https://api.chucknorris.io/jokes/random',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
+
+
+// $.ajax({
+//   url: 'https://api.chucknorris.io/jokes/random',
+//   dataType: 'json',
+//   success: function(data) {
+//     console.log(data);
+//   }
+// });
