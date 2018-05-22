6f5bbc17fa8eb3256790a65f105d6b0e79004a88
stella <nerajno@gmail.com>
2018-05-22 08:12:10 -0400

    made the reload btn wrk

--- assets/js/chuck_norris.js
+++ assets/js/chuck_norris.js
@@ -1,3 +1,4 @@
+
 // create a variable for the joke-btn 
 //may need to review this idea
 var btn = document.getElementById("joke-btn");
@@ -7,7 +8,10 @@ var humbleRequest = new XMLHttpRequest();
 humbleRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
 humbleRequest.onload = function(){
     var humbleData = JSON.parse(humbleRequest.responseText);
+    var randomJoke = humbleData.value;
+    // $('.joke-to-change').html(randomJoke); next move
     console.log(humbleData.value);
+    
 };
 humbleRequest.send();
 // Begin accessing JSON data here
