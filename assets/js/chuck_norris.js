ba04f3c9161685f7f53f84346846398b40ae127c
stella <nerajno@gmail.com>
2018-05-24 19:21:12 -0400

    too tired to figure this out

--- assets/js/chuck_norris.js
+++ assets/js/chuck_norris.js
@@ -3,17 +3,19 @@
 //may need to review this idea
 var btn = document.getElementById("joke-btn");
 
+var randomJoke = '';
 // this should be fun
 var humbleRequest = new XMLHttpRequest();
 humbleRequest.open('GET', 'https://api.chucknorris.io/jokes/random');
 humbleRequest.onload = function(){
     var humbleData = JSON.parse(humbleRequest.responseText);
-    var randomJoke = humbleData.value;
+    randomJoke = humbleData.value;
     // $('.joke-to-change').html(randomJoke); next move
     console.log(humbleData.value);
     
 };
 humbleRequest.send();
+document.getElementById('joke-to-change').innerHTML = randomJoke;
 // Begin accessing JSON data here
 
 
